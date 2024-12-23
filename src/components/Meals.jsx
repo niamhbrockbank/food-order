026ce39ac364a84fd325c9a6da2af1f286e1672a
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-12-23 10:26:32 +0000

    Add meals section

---
+++ src/components/Meals.jsx
@@ -0,0 +1,10 @@
+export default function Meals() {
+  return (
+    <div id="meals">
+      <li>
+        <ul className="meal-item">list</ul>
+        <ul>of meals</ul>
+      </li>
+    </div>
+  );
+}
