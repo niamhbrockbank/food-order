9088de98de035df674e9179a68c5e09d4a6a2602
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-12-23 12:42:39 +0000

    Use context function

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,13 +1,25 @@
-export default function MealItem({ item }) {
-  const { image, name, price, description, selected } = item;
+import { useContext } from "react";
+import { CartContext } from "../store/CartContext";
+
+export default function MealItem({ item, selected }) {
+  const { addToCart } = useContext(CartContext);
+
+  const { id, image, name, price, description } = item;
+
   return (
     <article className="meal-item">
-      <img src={image} />
+      <img src="n/a" />
       <h3>{name}</h3>
       <p className="meal-item-price">${price}</p>
       <p className="meal-item-description">{description}</p>
       <span className="meal-item-actions">
-        {!selected && <button className="button">Add to Cart</button>}
+        {!selected ? (
+          <button className="button" onClick={() => addToCart(id)}>
+            Add to Cart
+          </button>
+        ) : (
+          <p>1</p>
+        )}
       </span>
     </article>
   );
