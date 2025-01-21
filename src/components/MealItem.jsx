71e21d4318781052a258a82332e643ce64dfbefe
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-01-21 16:09:51 +0000

    Format prices consistently

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import { CartContext } from "../store/CartContext";
+import { currencyFormatter } from "../utils/formatting";
 import ItemQuantity from "./ItemQuantity";
 
 export default function MealItem({ meal }) {
   const { addToCart, isInCart } = useContext(CartContext);
 
   const { id, image, name, price, description } = meal;
+  const formattedPrice = currencyFormatter.format(price);
   const alreadyInCart = isInCart(id);
 
   return (
@@ -14,7 +16,7 @@ export default function MealItem({ meal }) {
         <div>
           <img src={`http://localhost:3000/${image}`} alt={name} />
           <h3>{name}</h3>
-          <p className="meal-item-price">${price}</p>
+          <p className="meal-item-price">{formattedPrice}</p>
           <p className="meal-item-description">{description}</p>
         </div>
         <span className="meal-item-actions">
