9e7755e02b73c7324ae242e80f4aa698ea9cf89a
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-01-31 16:28:01 +0000

    Show number of cart items in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
+import { useContext } from "react";
 import diningImg from "../assets/logo.jpg";
+import CartContext from "../store/CartContext";
 import Button from "./UI/button";
 
 export default function Header({ openCartModal }) {
+  const { items } = useContext(CartContext);
+  const totalCartItems = items.reduce(
+    (totalNumberOfItems, item) => totalNumberOfItems + item.quantity,
+    0
+  );
   return (
     <div id="main-header">
       <span id="title">
         <img src={diningImg} /> <h1>Food Order</h1>
       </span>
       <Button textOnly onClick={openCartModal}>
-        Cart (0)
+        Cart ({totalCartItems})
       </Button>
     </div>
   );
