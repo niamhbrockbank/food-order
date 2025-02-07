2ce8fe1879b3462fc3a45d63ab38b04bcf0ea6a5
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-02-07 15:56:01 +0000

    Send simplest POST request with data to backend

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -23,8 +23,20 @@ export default function Checkout() {
     event.preventDefault();
 
     const fd = new FormData(event.target);
-    const data = Object.fromEntries(fd.entries());
-    console.log(data);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
 
     event.target.reset();
   }
@@ -39,12 +51,12 @@ export default function Checkout() {
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="Email" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
 
         <div className="control-row">
-          <Input label="Post Code" type="text" id="post-code" />
+          <Input label="Post Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
 
